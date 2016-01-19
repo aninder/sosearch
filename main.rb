1596598971ea0249c60ecaffc44020311fff7b14
aninder <aninder@gmail.com>
2016-01-19 20:09:47 +0530

    init

---
+++ main.rb
@@ -0,0 +1,30 @@
+require 'active_record'
+require 'sqlite3'
+ActiveRecord::Base.establish_connection(
+   adapter: "sqlite3",
+   database: "db/soruby.sqlite",
+   pool: "5",
+   timeout: "10000"
+)
+
+ActiveRecord::Base.class_eval do
+  m = method(:inherited)
+  define_method self.inherited do |c|
+    c.instance_eval do
+      def hello
+        puts "hi!!"
+      end
+      m.bind(self).call(c)
+    end
+  end
+end
+class User < ActiveRecord::Base
+end
+class Comment < ActiveRecord::Base
+end
+class Post < ActiveRecord::Base
+  has_many :children, :class_name=>"Post", :foreign_key=>"ParentId", :primary_key => "Id"
+end
+class Search < ActiveRecord::Base
+  self.table_name= "searchIndex"
+end
